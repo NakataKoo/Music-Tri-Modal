deb1c36f5d411182b72b7c770244abc10d4e249b
NakataKoo <aytyk5_0220@yahoo.co.jp>
2024-08-21 17:45:47 +0900

    fix model config

--- muscall/datasets/audiocaption.py
+++ muscall/datasets/audiocaption.py
@@ -10,7 +10,7 @@ import glob
 import librosa
 
 class AudioCaptionMidiDataset(Dataset):
-    def __init__(self, config, tokenizer=None, dataset_type="train"):
+    def __init__(self, config, tokenizer=None, dataset_type="train", midi_dic):
         """Constructs an AudioCaptionMidiDataset dataset.
 
         Args:
@@ -26,7 +26,7 @@ class AudioCaptionMidiDataset(Dataset):
         self._dataset_type = dataset_type
         self._data_dir = self.config.data_dir # ${env.data_root}/datasets/${dataset_config.dataset_name}
 
-        self.CP = CP(dict="/content/drive/MyDrive/Music-Tri-Modal/muscall/modules/MidiBERT/CP.pkl")
+        self.CP = CP(dict=midi_dic)
 
         self.dataset_json = os.path.join(
             self._data_dir, "dataset_{}.json".format(self._dataset_type)
