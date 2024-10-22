b43aca9883479d444ac890ba6da668cf4135c421
NakataKoo <aytyk5_0220@yahoo.co.jp>
2024-10-22 13:49:11 +0000

    fix training.yaml

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True 
 #export NCCL_SHM_DISABLE=1
 export NCCL_DEBUG=INFO
-export NCCL_IB_DISABLE=1
+#export NCCL_IB_DISABLE=1
 #python scripts/train.py 2> warnings.txt \
 python scripts/train.py \
---devices="0"
+--devices="0, 1"
