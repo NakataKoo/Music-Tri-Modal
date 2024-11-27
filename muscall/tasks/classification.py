0c626e004802f15e9b6f984e37fdbbac291a0597
NakataKoo <aytyk5_0220@yahoo.co.jp>
2024-11-27 11:54:17 +0000

    fix zeroshot

--- muscall/tasks/classification.py
+++ muscall/tasks/classification.py
@@ -11,11 +11,40 @@ def prepare_labels(labels, prompt=None):
     text_prompts = []
     for i, label in enumerate(labels):
         if prompt == "pianist8":
-            text_to_tokenize = "This piece of music is composed by {}.".format(label)
+            if label == "Bethel":
+                text_to_tokenize = "This music was created by the band Bethel Music, an American worship music collective based in California, known for its contemporary sound and strong Christian themes."
+            elif label == "Clayderman":
+                text_to_tokenize = "This music was performed by Richard Clayderman, a French pianist known for his romantic and sentimental style, whose repertoire includes a mix of original compositions, classical pieces, and popular music covers."
+            elif label == "Einaudi":
+                text_to_tokenize = "This music was composed by Ludovico Einaudi, an Italian pianist and composer who is known for his minimalist and meditative music that often incorporates elements of classical, rock, and electronic music."
+            elif label == "Hancock":
+                text_to_tokenize = "This music was composed by Herbie Hancock, an American jazz pianist and composer who is known for his innovative approach to jazz music, and for incorporating elements of funk, rock, and electronic music into his compositions."
+            elif label == "Hillsong":
+                text_to_tokenize = "This music was created by Hillsong, an Australian-based worship music collective that has become one of the most well-known and influential Christian music groups in the world, characterized by its powerful lyrics, modern sound, and uplifting messages of faith and hope."
+            elif label == "Hisaishi":
+                text_to_tokenize = "This music was composed by Hisaishi Joe, a Japanese composer known for his emotionally deep music that often features a blend of classical and traditional Japanese elements, and who has worked on numerous films and anime soundtracks."
+            elif label == "Ryuichi":
+                text_to_tokenize = " This music was composed by Ryuichi Sakamoto, a Japanese musician and composer known for his eclectic and experimental approach to music, which often blends elements of classical, electronic, and traditional Japanese music, and who has worked on a variety of projects including film scores, solo albums, and collaborations with other musicians."
+            elif label == "Yiruma":
+                text_to_tokenize = "This music was composed by Yiruma, a South Korean pianist and composer whose music has gained popularity through YouTube and social media, and whose style combines classical and contemporary elements with a strong emotional core."
         elif prompt == "vgmidi":
-            text_to_tokenize = "This piece of music is composed by {}.".format(label)
+            if label == "Joy":
+                text_to_tokenize = "This piece of music is a jubilant celebration that radiates a contagious sense of happiness and joy."
+            elif label == "Anger":
+                text_to_tokenize = ": This piece of music is a visceral experience that unleashes a torrent of anger and fear."
+            elif label == "Sadness":
+                text_to_tokenize = "This piece of music is a poignant reflection that evokes a deep sense of sadness and melancholy, taking the listener on an emotional journey through the depths of human experience."
+            elif label == "Calmness":
+                text_to_tokenize = "This piece of music is a soothing balm that washes over the listener with a gentle wave of calmness and tranquility."
         elif prompt == "emopia":
-            text_to_tokenize = "This piece of music is composed by {}.".format(label)
+            if label == "Joy":
+                text_to_tokenize = "This piece of music is a jubilant celebration that radiates a contagious sense of happiness and joy."
+            elif label == "Anger":
+                text_to_tokenize = ": This piece of music is a visceral experience that unleashes a torrent of anger and fear."
+            elif label == "Sadness":
+                text_to_tokenize = "This piece of music is a poignant reflection that evokes a deep sense of sadness and melancholy, taking the listener on an emotional journey through the depths of human experience."
+            elif label == "Calmness":
+                text_to_tokenize = "This piece of music is a soothing balm that washes over the listener with a gentle wave of calmness and tranquility."
         else:
             text_to_tokenize = label
         text_prompts.append(text_to_tokenize)
